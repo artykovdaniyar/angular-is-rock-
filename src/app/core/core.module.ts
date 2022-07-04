e7e0659ff1bf1ef31f3313714cba2fe7277846a9
Daniir Artykov <artykovdaniyar@gmail.com>
2022-07-04 14:58:38 +0600

    breadcrumbs' layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, BreadcrumbsComponent],
+  exports: [HeaderComponent, BreadcrumbsComponent],
   imports: [CommonModule, FontAwesomeModule],
 })
 export class CoreModule {}
