4cf6394941239cb046da8588e5b7ebb6beaeeb46
Daniir Artykov <artykovdaniyar@gmail.com>
2022-07-04 17:27:06 +0600

    implementation of ngOnInit hoot in courses.component.ts

--- src/app/core/courses/courses.component.ts
+++ src/app/core/courses/courses.component.ts
@@ -6,33 +6,37 @@ import { Course } from './models/course';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
 })
-export class CoursesComponent {
-  courses: Course[] = [
-    {
-      name: 'Video Course 1. Name Tag',
-      id: 1,
-      description:
-        "Learn about where you cna  find description, what information they include, how they work, and deatils about various components od a course description. Courses description report information about a university or college's classes. They're published both in coures catagogs thst outline degree requirements and in course schedules thst contain descriptions for all courses offered during",
-      duration: '1h 28 min',
-      date: '9 Nov 2018',
-    },
-    {
-      name: 'Video Course 1. Name Tag',
-      id: 2,
-      description:
-        "Learn about where you cna  find description, what information they include, how they work, and deatils about various components od a course description. Courses description report information about a university or college's classes. They're published both in coures catagogs thst outline degree requirements and in course schedules thst contain descriptions for all courses offered during",
-      duration: '1h 28 min',
-      date: '9 Nov 2018',
-    },
-    {
-      name: 'Video Course 1. Name Tag',
-      id: 3,
-      description:
-        "Learn about where you cna  find description, what information they include, how they work, and deatils about various components od a course description. Courses description report information about a university or college's classes. They're published both in coures catagogs thst outline degree requirements and in course schedules thst contain descriptions for all courses offered during",
-      duration: '1h 28 min',
-      date: '9 Nov 2018',
-    },
-  ];
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [];
+  ngOnInit(): void {
+    this.courses = [
+      {
+        name: 'Video Course 1. Name Tag',
+        id: 1,
+        description:
+          "Learn about where you cna  find description, what information they include, how they work, and deatils about various components od a course description. Courses description report information about a university or college's classes. They're published both in coures catagogs thst outline degree requirements and in course schedules thst contain descriptions for all courses offered during",
+        duration: '1h 28 min',
+        date: '9 Nov 2018',
+      },
+      {
+        name: 'Video Course 1. Name Tag',
+        id: 2,
+        description:
+          "Learn about where you cna  find description, what information they include, how they work, and deatils about various components od a course description. Courses description report information about a university or college's classes. They're published both in coures catagogs thst outline degree requirements and in course schedules thst contain descriptions for all courses offered during",
+        duration: '1h 28 min',
+        date: '9 Nov 2018',
+      },
+      {
+        name: 'Video Course 1. Name Tag',
+        id: 3,
+        description:
+          "Learn about where you cna  find description, what information they include, how they work, and deatils about various components od a course description. Courses description report information about a university or college's classes. They're published both in coures catagogs thst outline degree requirements and in course schedules thst contain descriptions for all courses offered during",
+        duration: '1h 28 min',
+        date: '9 Nov 2018',
+      },
+    ];
+  }
+
   deleteCourseHandler(course: Course) {
     console.log(course.id);
   }
