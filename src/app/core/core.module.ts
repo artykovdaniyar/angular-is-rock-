686347997ec2da46e3982b202bd39d8bec228352
Daniir Artykov <artykovdaniyar@gmail.com>
2022-07-04 15:32:56 +0600

    footer section's layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, BreadcrumbsComponent],
-  exports: [HeaderComponent, BreadcrumbsComponent],
+  declarations: [HeaderComponent, BreadcrumbsComponent, FooterComponent],
+  exports: [HeaderComponent, BreadcrumbsComponent, FooterComponent],
   imports: [CommonModule, FontAwesomeModule],
 })
 export class CoreModule {}
