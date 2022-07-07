e37333434681f22f721ae264399bdf4e555f8fe3
Daniir Artykov <artykovdaniyar@gmail.com>
2022-07-07 12:30:04 +0600

    Implementation of courses server and getCourses method

--- src/app/core/courses/courses.component.ts
+++ src/app/core/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from './courses.service';
 import { Course } from './models/course';
 
 @Component({
@@ -9,45 +10,9 @@ import { Course } from './models/course';
 export class CoursesComponent implements OnInit {
   courses: Course[] = [];
   searchValue = '';
+  constructor(private courseService: CoursesService) {}
   ngOnInit(): void {
-    this.courses = [
-      {
-        name: 'Angular 9. Теория и Практика 2020.',
-        id: 1,
-        description:
-          'Полное руководство для разработки крутейших динамических приложений. От глубокой теории до практики на Angular. Получите полное понимание того, как Angular работает в деталях и научитесь их использовать',
-        duration: 1053,
-        date: 1656871200000,
-        topRated: false,
-      },
-      {
-        name: 'Angular - The Complete Guide',
-        id: 2,
-        description:
-          'Develop modern, complex, responsive and scalable web applications with Angular 14. Fully understand the architecture behind an Angular application and how to use it. Use the gained, deep understanding of the Angular fundamentals to quickly establish yourself as a frontend developer. Create single-page applications with one of the most modern JavaScript frameworks out there',
-        duration: 2092,
-        date: 1541700000000,
-        topRated: true,
-      },
-      {
-        name: '100 Angular Challenge',
-        id: 3,
-        description:
-          'We will master all Angular has to offer by building 100 re-usable and practical Components, Directives, Services, Pipes and much more to be used in your personal or professional projects. Not only will we build 100 items, but we will also go over Jasmine and Unit Testing so we can write the most solid code possible and certify it does what it is supposed to.',
-        duration: 120,
-        date: 1658512800000,
-        topRated: false,
-      },
-      {
-        name: 'The Complete Angular Course: Beginner to Advanced',
-        id: 4,
-        description:
-          "Angular is one of the most popular frameworks for building client apps with HTML, CSS and TypeScript. If you want to establish yourself as a front-end or a full-stack developer, you need to learn Angular. If you've been confused or frustrated jumping from one Angular 4 tutorial to another, you've come to the right place.",
-        duration: 40,
-        date: 1558612800000,
-        topRated: false,
-      },
-    ];
+    this.courses = this.courseService.getCourses();
   }
   searchHandler(searchValue: string) {
     this.searchValue = searchValue;
